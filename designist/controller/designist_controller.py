f1c67afa9a059347e6611c5950385e8c63953a03
lst20082008 <sglst123@gmail.com>
2018-12-03 23:49:47 +0800

    添加数据库以及文章模板

--- designist/controller/designist_controller.py
+++ designist/controller/designist_controller.py
@@ -9,37 +9,15 @@ from designist.model.Category import Category
 def index():
     posts = Post.query.limit(9).all()
     print(posts)
-    return render_template('index2.html',posts=posts)
+    return render_template('index.html',posts=posts)
 
 @app.route('/show_regist')
 def show_regist():
     return render_template('show_regist.html')
 
-@app.route('/regist',methods=['POST'])
-def regist():
-    u = User(request.form['UserName'],request.form['Password'],request.form['telephone'],request.form['email'])
-    print('尝试注册用户，名字：',u.username,'密码：',u.password,'电话：',u.telphone,'邮箱：',u.email)
-    try:
-        db.session.add(u)
-        db.session.commit()
-    except BaseException as e:
-        print('注册失败:',e)
-        return "注册失败"+e.__str__()
-    return redirect(url_for('index'))
-
 @app.route('/show_login')
 def show_login():
     return render_template('show_login.html')
-
-@app.route('/login',methods=['POST'])
-def login():
-    if request.form['Password'] == User.query.filter_by(username=request.form['UserName']).first().password:
-        session['username']=request.form['UserName']
-        print('登陆成功')
-    else:
-        print('登陆失败')
-        return "登陆失败"
-    return redirect(url_for('index'))
 
 @app.route('/logout')
 def logout():
@@ -49,6 +27,55 @@ def logout():
 @app.route('/show_category')
 def show_category():
     return render_template('show_category.html')
+
+@app.route('/show_add_post')
+def show_add_post():
+    return render_template('show_add_post.html')
+
+@app.route('/show_post/<int:post_id>')
+def show_post(post_id):
+    post = Post.query.filter_by(id=post_id).first()
+    print(post)
+    return render_template('post.html',post = post)
+
+@app.route('/kb')
+def kb():
+    page = request.args.get('page',1,type = int)
+    category_id = request.args.get('category_id',0,type = int)
+    fatherCategory = Category.query.filter_by(father_id=0).all()
+    allCategory = Category.query.all()
+    if category_id == 0:
+        pagination = Post.query.order_by(Post.date).paginate(page,per_page=10,error_out=True)
+    else:
+        pagination = Post.query.filter_by(category_id = category_id).order_by(Post.date).paginate(page,per_page=10,error_out=True)
+    return render_template('kb.html',fatherCategory=fatherCategory,allCategory=allCategory,pagination=pagination)
+#Post##################################################################
+
+@app.route('/regist',methods=['POST'])
+def regist():
+    u = User(request.form['UserName'],request.form['Password'],None,request.form['email'])
+    print('尝试注册用户，名字：',u.username,'密码：',u.password,'电话：',None,'邮箱：',u.email)
+    try:
+        db.session.add(u)
+        db.session.commit()
+    except BaseException as e:
+        print('注册失败:',e)
+        return "注册失败"+e.__str__()
+    return redirect(url_for('index'))
+
+@app.route('/post',methods=['POST'])
+def post():
+    p = Post(request.form['title'],request.form['abstract'],request.form['content'],request.form['category_id'])
+    f = request.files['image']
+    print(os.path.join(os.path.abspath('.'),'designist/static/img/'+p.image))
+    f.save(os.path.join(os.path.abspath('.'),'designist/static/img/'+p.image))
+    try:
+        db.session.add(p)
+        db.session.commit()
+    except BaseException as e:
+        print('注册失败:',e)
+        return "注册失败"+e.__str__()
+    return redirect(url_for('index'))
 
 @app.route('/category',methods=['POST'])
 def category():
@@ -61,28 +88,12 @@ def category():
         return "注册失败"+e.__str__()
     return redirect(url_for('index'))
 
-@app.route('/show_add_post')
-def show_add_post():
-    return render_template('show_add_post.html')
-
-@app.route('/post',methods=['POST'])
-def post():
-    p = Post(request.form['title'],request.form['abstract'],request.form['content'],datetime.datetime.now(),request.form['category_id'])
-    f = request.files['image']
-    print(os.path.join(os.path.abspath('.'),'designist/static/img/'+p.image))
-    f.save(os.path.join(os.path.abspath('.'),'designist/static/img/'+p.image))
-    try:
-        db.session.add(p)
-        db.session.commit()
-    except BaseException as e:
-        print('注册失败:',e)
-        return "注册失败"+e.__str__()
+@app.route('/login',methods=['POST'])
+def login():
+    if request.form['Password'] == User.query.filter_by(username=request.form['UserName']).first().password:
+        session['username']=request.form['UserName']
+        print('登陆成功')
+    else:
+        print('登陆失败')
+        return "登陆失败"
     return redirect(url_for('index'))
-
-@app.route('/show_post')
-def show_post():
-    fatherCategory = Category.query.filter_by(father_id='0').all()
-    allCategory = Category.query.all()
-    print(fatherCategory)
-    print(allCategory)
-    return render_template('show_post.html',fatherCategory=fatherCategory,allCategory=allCategory)
--- designist/model/Post.py
+++ designist/model/Post.py
@@ -1,4 +1,5 @@
 from designist import db
+import datetime
 
 class Post(db.Model):
     __tablename__ = 'post'
@@ -9,15 +10,17 @@ class Post(db.Model):
     content = db.Column(db.Text,nullable=False)
     date = db.Column(db.DateTime,nullable=False)
     likes = db.Column(db.Integer)
+    reads = db.Column(db.Integer)
     category_id = db.Column(db.Integer,db.ForeignKey('category.id'),nullable=False)
 
-    def __init__(self,title,abstract,content,date,category_id):
+    def __init__(self,title,abstract,content,category_id):
         self.title  = title
         self.abstract = abstract
         self.content = content
-        self.date = date
+        self.date = datetime.datetime.now()
         self.category_id = category_id
         self.image = 'post_'+title+'.jpg'
         self.likes = 0
+        self.reads = 0
     def __repr__(self):
-        return '<User %r>' % self.title
+        return '<Post %r>' % self.title
